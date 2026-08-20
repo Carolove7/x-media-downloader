d3f4239594fbf3730a6ba8d6ff10ea2d3cbfbf2d
Carolove7 <1964136216@qq.com>
2026-08-21 04:21:18 +0800

    fix: import Emitter trait in downloader.rs

--- src-tauri/src/downloader.rs
+++ src-tauri/src/downloader.rs
@@ -6,7 +6,7 @@ use std::collections::HashSet;
 use std::path::PathBuf;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
-use tauri::{AppHandle, Manager};
+use tauri::{AppHandle, Emitter, Manager};
 use tokio::fs::{self, File};
 use tokio::io::AsyncWriteExt;
 use tokio::sync::Semaphore;
